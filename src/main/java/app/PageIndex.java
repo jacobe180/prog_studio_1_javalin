bdc1191a580edf2165067b95f1d9ee20d0444682
Jacob Eisho <s3965880@student.rmit.edu.au>
2022-10-07 12:49:21 +1100

    Initialize with cosc2803-week05

---
+++ src/main/java/app/PageIndex.java
@@ -0,0 +1,100 @@
+package app;
+
+import java.util.ArrayList;
+
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+
+/**
+ * Example Index HTML class using Javalin
+ * <p>
+ * Generate a static HTML page using Javalin
+ * by writing the raw HTML into a Java String object
+ *
+ * @author Timothy Wiley, 2022. email: timothy.wiley@rmit.edu.au
+ * @author Santha Sumanasekara, 2021. email: santha.sumanasekara@rmit.edu.au
+ */
+public class PageIndex implements Handler {
+
+    // URL of this page relative to http://localhost:7001/
+    public static final String URL = "/";
+
+    @Override
+    public void handle(Context context) throws Exception {
+        // Create a simple HTML webpage in a String
+        String html = "<html>";
+
+        // Add some Head information
+        html = html + "<head>" + 
+               "<title>All Movies</title>";
+
+        // Add some CSS (external file)
+        html = html + "<link rel='stylesheet' type='text/css' href='common.css' />";
+        html = html + "</head>";
+
+        // Add the body
+        html = html + "<body>";
+
+        // Add the topnav
+        // This uses a Java v15+ Text Block
+        html = html + """
+            <div class='topnav'>
+                <a href='/'>Homepage</a>
+            </div>
+        """;
+
+        // Add header content block
+        html = html + """
+            <div class='header'>
+                <h1>
+                    <img src='logo.png' class='top-image' alt='RMIT logo' height='75'>
+                    Week 05 exercise index.html page
+                </h1>
+            </div>
+        """;
+
+        // Add Div for page Content
+        html = html + "<div class='content'>";
+
+        // Look up some information from JDBC
+        // First we need to use your JDBCConnection class
+        JDBCConnection jdbc = new JDBCConnection();
+
+        // Next we will ask this *class* for the movies
+        ArrayList<Movie> movies = jdbc.getMovies();
+
+        // Add HTML for the movies list
+        html = html + "<h1>All Movies</h1>" + "<ul>";
+
+        // Finally we can print out all of the movies
+        for (Movie movie : movies) {
+            html = html + "<li>" + movie.name + "</li>";
+        }
+
+        // Finish the List HTML
+        html = html + "</ul>";
+        
+        // TODO: Add your code here for the other queries such as 
+        //       countMovies(), getMoviesByType('horror'), horrorMovies()
+
+
+        // Close Content div
+        html = html + "</div>";
+
+        // Footer
+        html = html + """
+            <div class='footer'>
+                <p>COSC2803 Module 0 - Week 05</p>
+            </div>
+        """;
+
+        // Finish the HTML webpage
+        html = html + "</body>" + "</html>";
+
+
+        // DO NOT MODIFY THIS
+        // Makes Javalin render the webpage
+        context.html(html);
+    }
+
+}
